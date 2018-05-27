c2df33e05c9cfdb8654b29e085c512cf3e4c8f79
mdiannna <mdiannna@yahoo.com>
2018-05-27 18:19:12 +0300

    finished ex1

--- tema2/src/udp_client.py
+++ tema2/src/udp_client.py
@@ -6,7 +6,7 @@ import timeit
 import threading
 
 timeout = 3
-max_number = 12
+max_number = 6
 
 # Sliding window
 window_size = 3
@@ -51,42 +51,61 @@ class waitForConfirmation(threading.Thread):
       # number = i
 
     def run(self):
-        number = 0;
-        print("selfi: " + str(number));
-        global window_start
-        global window_end
-        global confirmedNumbers
+        while True:
+            number = 0;
+            print("selfi: " + str(number));
+            global window_start
+            global window_end
+            global confirmedNumbers
 
-        logging.info('Asteptam un raspuns...')
-        try:
-            data, server = sock.recvfrom(4096)
-            logging.info('Content primit: "%s"', data)
-            number = int(data.replace("Hello ", ""));
-            print "SELFI:" + number
 
-            confirmedNumbers.append(number);
+            logging.info('Asteptam un raspuns...')
+            try:
+                data, server = sock.recvfrom(4096)
+                logging.info('Content primit: "%s"', data)
+                number = int(data.replace("hello ", ""));
+                print "SELFI:" + str(number)
 
-            threadLock.acquire()
-            print("selfi: " + str(number) + "   window_start:" + str(window_start))
-            print("sent numbers:")
-            print confirmedNumbers
+                confirmedNumbers.append(number);
 
-            # daca numarul a fost trimis deja, incrementez window_start si window_end
-            while (window_end+1 in confirmedNumbers and window_end < max_number):
-                window_end = window_end + 1
-                window_start = window_start + 1
+                # threadLock.acquire()
+                print("selfi: " + str(number) + "   window_start:" + str(window_start))
+                print("sent numbers:")
+                print confirmedNumbers
 
-            if(window_end < max_number and number == window_start and window_end < max_number):
-                    window_start = window_start+1;
-                    window_end = window_end+1;
-                    print ("send" + str(window_end))
-                    sendI(window_end);
-                
-            threadLock.release()
 
-        except:
-            logging.info("timeout" + str(number))
-            sendI(number)
+                if(window_end >= max_number) and len(confirmedNumbers) == max_number:
+                    logging.info('All numbers received:')
+                    print confirmedNumbers
+                    return
+
+
+                # daca numarul a fost trimis deja, incrementez window_start si window_end
+                while (window_start+1 in confirmedNumbers and window_end < max_number):
+                    window_end = window_end + 1
+                    window_start = window_start + 1
+
+                if(window_end < max_number and number == window_start):
+                        window_start = window_start+1;
+                        window_end = window_end+1;
+                        print ("send" + str(window_end))
+                        sendNumber(window_end);
+                    
+                # threadLock.release()
+
+              
+            except:
+                j = 1
+                while j <= window_end:
+                    if j not in confirmedNumbers:
+                        break
+                    j = j+1
+                    print("j: " + str(j))
+                if(j > max_number):
+                    return
+                number = j
+                logging.info("timeout" + str(number))
+                sendNumber(number)
 
 
 
@@ -95,27 +114,31 @@ threadLock = threading.Lock()
 threads = []
 
 
-def sendI(i):
-    sendNumber(i)
-    thread1 = waitForConfirmation()
-    thread1.start();
-    threads.append(thread1)
+# def sendNumber(i):
+# sendNumber(i)
+    # thread1 = waitForConfirmation()
+    # thread1.start();
+    # threads.append(thread1)
 
 
 i=1;
 
-for i in range(window_start, window_end+1):
-    sendNumber(i)
 
 initial_window_start = window_start
 initial_window_end = window_end
 
 for i in range(initial_window_start, initial_window_end+1):
-    thread1 = waitForConfirmation()
-    thread1.start();
-    threads.append(thread1)
+    sendNumber(i)
 
-    # sendI(i);
+
+thread1 = waitForConfirmation()
+thread1.start();
+threads.append(thread1)
+
+
+# for i in range:
+
+    # sendNumber(i);
 
 
 
@@ -133,8 +156,8 @@ for i in range(initial_window_start, initial_window_end+1):
 
 # finally:
 
-for t in threads:
-    t.join()
+# for t in threads:
+thread1.join()
 
 logging.info('window start:' + str(window_start))
 logging.info('window end:' + str(window_end))
